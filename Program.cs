44db5fef1311c2b8b711fee5d3ef8469e887b717
Chamiya09 <chamodekanayaka27@gmail.com>
2026-09-09 23:53:43 +0530

    fix: disable HTTPS redirection in development environment

--- Program.cs
+++ Program.cs
@@ -129,7 +129,10 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors("AllowFrontend");
 
--- obj/Debug/net8.0/Skill-Hub-BackEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/Skill-Hub-BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Skill-Hub-BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7980b1f78d310557fb4683314a54c2cd394fe1c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cfc2364a1c0f3150466b96f11692b2c84e2ffd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Skill-Hub-BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Skill-Hub-BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
