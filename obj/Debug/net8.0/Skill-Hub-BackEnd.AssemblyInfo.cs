d4276411d4aaf9600045ff2884b2e8c5552de6db
DinusaraNethmal <dinusaranethmal@gmail.com>
2026-09-18 21:00:18 +0530

    Backend

--- obj/Debug/net8.0/Skill-Hub-BackEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/Skill-Hub-BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Skill-Hub-BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9d0eb924e82219419413b9897084425e744273")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5687afd9ce36bb0b45abee7d9ac4042b4697afff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Skill-Hub-BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Skill-Hub-BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
