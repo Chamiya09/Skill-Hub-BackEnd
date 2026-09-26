c4523b460ade6a51587a2d4648f0aea7e0982286
amandagangamini <amandagangamini@gmail.com>
2026-09-26 21:31:33 +0530

    Update assembly informational version and cache for build consistency

--- obj/Debug/net8.0/Skill-Hub-BackEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/Skill-Hub-BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Skill-Hub-BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f4949d88ea8b7b511b856ced6bd34338d71a26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7081166916eaa8a36d23c7ef60b497b69e0afc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Skill-Hub-BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Skill-Hub-BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
